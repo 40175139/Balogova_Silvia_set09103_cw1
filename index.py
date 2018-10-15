f293e432986ecf18c42b94034ef775c73ef6aeae
NAPIER-MAIL\40175139 <40175139@live.napier.ac.uk>
2018-10-15 13:18:46 +0100

    basic setup

--- index.py
+++ index.py
@@ -1,17 +1,20 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route("/")
-def main():
-	return 'I am the main page'
-
-@app.route("/chocolate/")
-def chocolate():
-	return 'I am the page about chocolate'
-
+@app.rpute("/<name>")
+@app.route("/home/")
 @app.route("/about/")
-def about():
-	return 'I am the page about about'
+def home(name=None):
+	return render_template('home.html', name=name)
+
+@app.route("/countries/")
+def countries():
+	return render_template('countries.html')
+
+@app.route("/contact/")
+def contact():
+	return render_template('contact.html')
 
 if __name__ == "__main__":
 	app.run(host='0.0.0.0', debug=True)

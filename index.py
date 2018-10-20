0977bff0b7ae958433a1db693ccc44f411c3bad1
40175139 <40175139@live.napier.ac.uk>
2018-10-21 00:31:13 +0100

    second working version
    added sorting by continent, pics, json data for testing, css improvements

--- index.py
+++ index.py
@@ -8,6 +8,7 @@ json_file = open("country_data.json").read()
 country_list = json.loads(json_file)
 
 
+
 @app.route("/")
 @app.route("/home/")
 @app.route("/about/")
@@ -16,7 +17,8 @@ def home(name=None):
 
 @app.route("/countries/")
 def countries():
-	return render_template('countries.html')
+	countries = sort_by_continent(country_list)
+	return render_template('countries.html', countries=countries)
 
 @app.route("/contact/")
 def contact():
@@ -29,12 +31,26 @@ def country_detail(name):
 	for country in country_list:
 		if country["name"].lower() == name.lower():
 			return render_template('countries_detail.html', country=country)
-	abort(404)
+	return "Not loading country detail."
+	#abort(404)
 
 
 @app.errorhandler(404)
 def error_404(error):
 	return render_template('error.html'), 404
+
+def sort_by_continent(list):
+	continents = { }
+
+	for country in country_list:
+		continent = country["continent"]
+
+		if continent not in continents:
+			continents[continent] = []
+
+		continents[continent].append(country)
+
+	return continents
 
 if __name__ == "__main__":
 	app.run(host='0.0.0.0', debug=True)
